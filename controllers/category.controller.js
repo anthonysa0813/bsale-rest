f5fa6abeeae39820536fda1965163da21aff2cf0
anthony <anthonysa0813@gmail.com>
2022-10-17 19:03:02 -0500

    added tryCatch in the controllers

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -2,13 +2,29 @@ const { response, request } = require("express");
 const pool = require("../database/config");
 
 const getAllCategories = async (req = request, res = response) => {
-  const categories = await pool.query("SELECT * FROM category");
-  res.json(categories);
+  try {
+    const categories = await pool.query("SELECT * FROM category");
+    return res.json(categories[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: "something goes wrong",
+    });
+  }
 };
 
 const getCategoryById = async (req = request, res = response) => {
-  const category = await pool.query("SELECT * FROM category WHERE id = 1");
-  res.json(category);
+  try {
+    const { id } = req.params;
+    const category = await pool.query(
+      "SELECT * FROM category WHERE id = ?",
+      id
+    );
+    return res.json(category[0]);
+  } catch (error) {
+    return res.status(500).json({
+      messsage: "something goes wrong",
+    });
+  }
 };
 
 module.exports = {
--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -5,28 +5,33 @@ const getAllProducts = async (req = request, res = response) => {
   const query = req.query;
   const { name } = query;
   const products = await pool.query("SELECT * FROM product");
-  if (name) {
-    const productFilter = await pool.query(
-      "SELECT * FROM product WHERE name LIKE ?",
-      `%${name}%`
-    );
-    return res.json(productFilter[0]);
+  try {
+    if (name) {
+      const productFilter = await pool.query(
+        "SELECT * FROM product WHERE name LIKE ?",
+        `%${name}%`
+      );
+      return res.json(productFilter[0]);
+    }
+    return res.json(products[0]);
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong",
+    });
   }
-  res.json(products[0]);
 };
 
 const getProductsByIdCategory = async (req = request, res = response) => {
   const { categoryId } = req.params;
-  console.log("categoryId", categoryId);
-  const products = await pool.query(
-    `SELECT * FROM product  WHERE category = (?)`,
-    [categoryId]
-  );
-  res.json(products[0]);
-
-  // res.json({
-  //   categoryId,
-  // });
+  try {
+    const products = await pool.query(
+      `SELECT * FROM product  WHERE category = (?)`,
+      [categoryId]
+    );
+    return res.json(products[0]);
+  } catch (error) {
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 module.exports = {
